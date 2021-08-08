8125f3547b76058503e8344c86cdbf06b6f8366a
ZanxinZ <47962594+ZanxinZ@users.noreply.github.com>
2021-08-08 17:09:10 +0800

    Update UI

---
+++ app/src/main/java/com/example/caketouch/DishDetailFragment.java
@@ -0,0 +1,7 @@
+package com.example.caketouch;
+
+import android.support.v4.app.DialogFragment;
+
+public class DishDetailFragment extends DialogFragment {
+
+}
--- app/src/main/java/com/example/caketouch/NewDishActivity.java
+++ app/src/main/java/com/example/caketouch/NewDishActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
