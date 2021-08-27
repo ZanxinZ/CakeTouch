88828bbf9a4e446c3fe456ee956db62a5f09cc68
ZanxinZ <47962594+ZanxinZ@users.noreply.github.com>
2021-08-27 18:35:47 +0800

    Update OrderedShow
    Serve food In table detail.

--- app/src/main/java/com/example/caketouch/fragment/TableDetailDialogFragment.java
+++ app/src/main/java/com/example/caketouch/fragment/TableDetailDialogFragment.java
@@ -104,7 +104,7 @@ public class TableDetailDialogFragment extends DialogFragment {
                                 .setNegativeButton("取消", new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int which) {
-                                        dismiss();
+                                        //constructSmallStuffs(linearLayout, linearLayoutServed);
                                     }
                                 });
                         serveFoodDialog.show();
