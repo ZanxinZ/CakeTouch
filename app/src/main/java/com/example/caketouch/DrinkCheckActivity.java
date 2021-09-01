554a773f01acf17b73ed65021162b44c031c13a2
ZanxinZ <47962594+ZanxinZ@users.noreply.github.com>
2021-09-02 02:17:08 +0800

    Update drinkSmallBlock height

--- app/src/main/java/com/example/caketouch/DrinkCheckActivity.java
+++ app/src/main/java/com/example/caketouch/DrinkCheckActivity.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.example.caketouch.food_for_serve.DrinkOrdered;
@@ -54,6 +55,7 @@ public class DrinkCheckActivity extends Activity {
         View drinkBlock = inflater.inflate(R.layout.drink_block,null);
         TextView textViewTableNo = drinkBlock.findViewById(R.id.textViewDrinkBlockTableNo);
         textViewTableNo.setText(tableNo + " " + textViewTableNo.getText());
+        //ScrollView scrollView =
         LinearLayout linearLayoutDrinkBlocks = drinkBlock.findViewById(R.id.drink_blocks);//drinkSmallBlocks
         linearLayoutDrinkBlocks.removeAllViews();
         for (Map.Entry<Long, DrinkOrdered> drinkOrderedEntry:
